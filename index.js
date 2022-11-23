067db5d8ed092e2dbecc1fb0d0f2966b37fd6c9c
Marcelle Vukoja <booksread555@gmail.com>
2022-11-23 16:12:02 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+// Removes main tag/node element
+document.querySelector("main#main").remove();
+
+// Create new element assign as newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Marcelle is the champion!";
